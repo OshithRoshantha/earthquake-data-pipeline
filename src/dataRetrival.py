7f41e95a114e4c6c017b2e82893e5974ea3809df
Oshith Roshantha <oedirisuriya@gmail.com>
2024-11-23 23:41:06 +0530

    Add data retrieval and transformation scripts for earthquake data

---
+++ src/dataRetrival.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime,timedelta
+
+def fetchFromApi(startTime,endTime):
+    params={
+        "format":'geojson',
+        "starttime":startTime,
+        "endtime":endTime,
+        "minmagnitude":4
+    }
+    response=requests.get('https://earthquake.usgs.gov/fdsnws/event/1/query',params=params)
+    if response.status_code==200:
+        return response.json()
+    else:
+        print('Unable to fetch data')
+        
+def setTime(executeTime):
+    startTime=executeTime-timedelta(hours=1)
+    return startTime.strftime('%Y-%m-%dT%H:%M:%S'),executeTime.strftime('%Y-%m-%dT%H:%M:%S')
+    
