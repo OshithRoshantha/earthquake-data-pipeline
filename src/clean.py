1fa7e3c992e6febb0b88c9f49b78257b87bed497
Oshith Roshantha <oedirisuriya@gmail.com>
2024-11-24 09:59:22 +0530

    Add preprocessing function for earthquake data extraction and transformation

---
+++ src/clean.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import json
+
+def preProcessing(rawData):
+    
+    features=rawData.get('features',[])
+    processedData=[]
+    
+    for feature in features:
+        prop=feature.get('properties',{})
+        geom=feature.get('geometry',{})
+        
+        earthqData={
+            "id":feature.get('id',None),
+            "place":prop.get('place',None),
+            "time":pd.to_datetime(prop.get("time",None),unit='ms',errors='coerce'),
+            "tsunami":prop.get('tsunami',None),
+            "sig":prop.get('sig',None),
+            "nst":prop.get('nst',None),
+            "dmin":prop.get('dmin',None),
+            "gap":prop.get('gap',None),
+            "magType":prop.get('magType',None),
+            "long":geom.get("coordinates",[None, None, None])[0],
+            "lat":geom.get("coordinates",[None, None, None])[1],
+            "depth":geom.get("coordinates",[None, None, None])[2],
+        }
