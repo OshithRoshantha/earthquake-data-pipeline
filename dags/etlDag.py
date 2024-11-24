480f15048f4105e36ea43061a066ba48c256c587
Oshith Roshantha <oedirisuriya@gmail.com>
2024-11-24 18:43:07 +0530

    Refactor ETL DAG to import modules from src package; update function calls for data retrieval, preprocessing, transformation, and Azure push

--- dags/etlDag.py
+++ dags/etlDag.py
@@ -1,9 +1,7 @@
-import sys
-import os
 from datetime import datetime,timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from src import dataRetrival,clean,transform,pushToLake
 
 defaultArgs = {
     'owner': 'Oshith Roshantha',
@@ -29,22 +27,19 @@ def setTime(executionDate, prevExecutionDate, **kwargs):
     return startTime, endTime
 
 def fetchData(**kwargs):
-    import src
     executionDate = kwargs['execution_date']
     prevExecutionDate = kwargs.get('prev_execution_date')
     startTime, endTime = setTime(executionDate, prevExecutionDate, **kwargs)
-    rawData = src.fetchFromApi(startTime, endTime)
+    rawData = dataRetrival.fetchFromApi(startTime, endTime)
     return rawData
 
 def preprocessData(**kwargs):
-    import src
     taskInstance = kwargs['task_instance']
     rawData = taskInstance.xcom_pull(task_ids='fetchData')
-    processedData = src.preProcessing(rawData)
+    processedData = clean.preProcessing(rawData)
     return processedData
 
 def transformDataTask(**kwargs):
-    import src
     taskInstance = kwargs['task_instance']
     processedData = taskInstance.xcom_pull(task_ids='preprocessData')
     
@@ -52,14 +47,13 @@ def transformDataTask(**kwargs):
     scaler = previousTransformData['scalar'] if previousTransformData and 'scalar' in previousTransformData else None
     encoder = previousTransformData['encoder'] if previousTransformData and 'encoder' in previousTransformData else None
     
-    encodedData,  scaler, encoder = src.transformData(processedData, scaler, encoder)
+    encodedData,  scaler, encoder = transform.transformData(processedData, scaler, encoder)
     return {'encodedData': encodedData, 'scalar': scaler, 'encoder':encoder}
 
 def pushDataToAzure(**kwargs):
-    import src
     taskInstance = kwargs['task_instance']
     transformedData = taskInstance.xcom_pull(task_ids='transformDataTask')
-    src.pushToAzure(transformedData['encodedData'])
+    pushToLake.pushToAzure(transformedData['encodedData'])
 
 taskFetchData = PythonOperator(
     task_id='fetchData',
