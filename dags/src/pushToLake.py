dc1ca68b28e1681e1a9e68778ff7b661a3b1dcbb
Oshith Roshantha <oedirisuriya@gmail.com>
2024-11-26 00:02:49 +0530

    Streamline blob upload process and remove unnecessary CSV conversion

--- dags/src/pushToLake.py
+++ dags/src/pushToLake.py
@@ -1,7 +1,5 @@
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
-from io import BytesIO
-import pandas as pd
 import os
 
 load_dotenv()
@@ -12,14 +10,9 @@ containerClient2 = blobServiceClient.get_container_client('production-data')
 def pushToProduction():
     blobClient1 = containerClient1.get_blob_client('transform-data.parquet')
     blobClient2 = containerClient2.get_blob_client('train-data.parquet')
-    blobClient3 = containerClient2.get_blob_client('train-data.csv')
     downloadStream = blobClient1.download_blob()
-    parquetData = downloadStream.readall()
-    blobClient2.upload_blob(parquetData, overwrite=True)
-    
-    parquet_df = pd.read_parquet(BytesIO(parquetData))
-    csv_data = parquet_df.to_csv(index=False)
-    blobClient3.upload_blob(csv_data, overwrite=True)
+    blobClient2.upload_blob(downloadStream.readall(), overwrite=True)
+
     
     
     
